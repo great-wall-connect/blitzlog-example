777f7bb0eca25fa5ea1be3b07578f17825c6b5ce
daniel-sarosi-gwc <203321995+daniel-sarosi-gwc@users.noreply.github.com>
2026-08-19 06:19:04 +0000

    test: cover DELETE success and not-found paths

--- tests/integration.rs
+++ tests/integration.rs
@@ -11,7 +11,10 @@ async fn spawn_app() -> SocketAddr {
             "/tasks",
             axum::routing::get(handlers::list_tasks).post(handlers::create_task),
         )
-        .route("/tasks/:id", axum::routing::get(handlers::get_task))
+        .route(
+            "/tasks/:id",
+            axum::routing::get(handlers::get_task).delete(handlers::delete_task),
+        )
         .with_state(store);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
@@ -61,5 +64,36 @@ async fn get_missing_task_returns_404() {
     let addr = spawn_app().await;
     let url = format!("http://{}/tasks/00000000-0000-0000-0000-000000000000", addr);
     let resp = reqwest::get(url).await.unwrap();
+    assert_eq!(resp.status(), reqwest::StatusCode::NOT_FOUND);
+}
+
+#[tokio::test]
+async fn delete_task_removes_it() {
+    let addr = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let list_url = format!("http://{}/tasks", addr);
+    let resp = client.get(&list_url).send().await.unwrap();
+    let body: Vec<Value> = resp.json().await.unwrap();
+    assert_eq!(body.len(), 3);
+    let id = body[0]["id"].as_str().unwrap().to_string();
+
+    let delete_url = format!("http://{}/tasks/{}", addr, id);
+    let resp = client.delete(&delete_url).send().await.unwrap();
+    assert_eq!(resp.status(), reqwest::StatusCode::NO_CONTENT);
+
+    let resp = client.get(&delete_url).send().await.unwrap();
+    assert_eq!(resp.status(), reqwest::StatusCode::NOT_FOUND);
+
+    let resp = client.get(&list_url).send().await.unwrap();
+    let body: Vec<Value> = resp.json().await.unwrap();
+    assert_eq!(body.len(), 2);
+}
+
+#[tokio::test]
+async fn delete_missing_task_returns_404() {
+    let addr = spawn_app().await;
+    let url = format!("http://{}/tasks/00000000-0000-0000-0000-000000000000", addr);
+    let resp = reqwest::Client::new().delete(&url).send().await.unwrap();
     assert_eq!(resp.status(), reqwest::StatusCode::NOT_FOUND);
 }
